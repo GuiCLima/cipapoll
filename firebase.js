df7ec0a2b2b83733e96992a2379a54d56c4ec79a
GuiCLima <gui7play@gmail.com>
2024-01-10 17:23:28 -0300

    Reajustments

--- firebase.js
+++ firebase.js
@@ -1,32 +1,27 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-import {
-    getFirestore,
-    collection,
-    getDocs,
-    addDoc,
-    doc,
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getFirestore, collection, getDocs, updateDoc, doc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+import { apiKey, appId, authDomain, messagingSenderId, projectId, storageBucket } from "./config";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAVXps4dqu3q4yUNLJLZT2lo6CMsPUvqOM",
-    authDomain: "cozilpoll.firebaseapp.com",
-    projectId: "cozilpoll",
-    storageBucket: "cozilpoll.appspot.com",
-    messagingSenderId: "190251371277",
-    appId: "1:190251371277:web:48909657d5a6d95de27e44"
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-async function find(cpf) {
+export async function find(cpf) {
     const querySnapshot = await getDocs(collection(db, 'colaboradores'));
     let index = 0;
 
-
-    return await new Promise((resolve, reject) => {
+    return await new Promise((resolve) => {
         querySnapshot.forEach((doc) => {
             if (doc.data().cpf === cpf) {
                 resolve({
@@ -43,26 +38,44 @@ async function find(cpf) {
     });
 }
 
+export async function getCandidates(type) {
+    const querySnapshot = await getDocs(collection(db, 'candidato'));
+    let index = 0;
 
+    return await new Promise((resolve) => {
+        querySnapshot.forEach((doc) => {
+            if(doc.id === type) {
+                resolve(doc.data());
+            }
 
-export async function hasVoted(cpf) {
-    const user = (await find(cpf)).data;
-
-    if (!user) {
-        return undefined;
-    }
-    return user.hasVoted;
+            if(querySnapshot.size - 1 === index) {
+                resolve(undefined);
+            }
+            index++;
+        })
+    })
 }
 
-export async function isCozil(cpf) {
-    const user = await (await find(cpf)).data;
-
-    if (!user) {
-        return undefined;
-    }
-    return user.isCozil;
+export async function updateUserVote(id) {
+    await updateDoc(doc(db, "colaboradores", id), {
+        hasVoted: true
+    });
 }
 
-function vote(cpf, vote) {
+export async function updateCandidateVote(id, key, previousValue) {
+    console.log(id);
+    console.log(key);
+    console.log(previousValue);
+    
+    await updateDoc(doc(db, "candidato", id), {
+        [key]: previousValue + 1
+    })
+}
 
+export async function hasVoted(user) {
+    return user.data.hasVoted;
+}
+
+export async function isCozil(user) {
+    return user.data.isCozil;
 }
--- votacao.js
+++ votacao.js
@@ -1,44 +1,210 @@
-import { hasVoted } from "./firebase.js";
+import { hasVoted, find, updateUserVote, getCandidates, updateCandidateVote } from "./firebase.js";
 
-const button = document.getElementById("next-button");
-button.addEventListener('click', moveCarousel);
+let currentPosition = 0;
+let selectedCandidate;
 
+let candidatesList = {};  
+let isCozil = true;
+
+window.onload = function() { 
+    setupButtons();
+};
+
+// SHOUTOUT TO: trincot (from stackoverflow)
+document.addEventListener('DOMContentLoaded', () => {
+    for (const el of document.querySelectorAll("[placeholder][data-slots]")) {
+        const pattern = el.getAttribute("placeholder"),
+            slots = new Set(el.dataset.slots || "_"),
+            prev = (j => Array.from(pattern, (c,i) => slots.has(c)? j=i+1: j))(0),
+            first = [...pattern].findIndex(c => slots.has(c)),
+            accept = new RegExp(el.dataset.accept || "\\d", "g"),
+            clean = input => {
+                input = input.match(accept) || [];
+                return Array.from(pattern, c =>
+                    input[0] === c || slots.has(c) ? input.shift() || c : c
+                );
+            },
+            format = () => {
+                const [i, j] = [el.selectionStart, el.selectionEnd].map(i => {
+                    i = clean(el.value.slice(0, i)).findIndex(c => slots.has(c));
+                    return i<0? prev[prev.length-1]: back? prev[i-1] || first: i;
+                });
+                el.value = clean(el.value).join``;
+                el.setSelectionRange(i, j);
+                back = false;
+            };
+        let back = false;
+        el.addEventListener("keydown", (e) => back = e.key === "Backspace");
+        el.addEventListener("input", format);
+        el.addEventListener("focus", format);
+        el.addEventListener("blur", () => el.value === pattern && (el.value=""));
+    }
+});
+
+function setupButtons() {
+    const buttons = document.getElementsByClassName("button");
+
+    for(let i=0; i<buttons.length; i++) {
+        buttons[i].addEventListener('click', moveCarousel);
+    }
+}
+
+async function loadCandidates() {
+    const wrapper = document.getElementById('candidate-wrapper');
+    const cpf = getCPF();
+    const user = await find(cpf);
+    let candidates = {};
+
+    wrapper.innerHTML = "";
+    
+    if(user.data.isCozil) {
+        candidates = await getCandidates("votosCOZIL");
+
+        for(const key in candidates) {
+            wrapper.appendChild(buildCandidateElement(key));
+        }
+    } else {
+        candidates = await getCandidates("votosPROINOX");
+
+        for(const key in candidates) {
+            wrapper.appendChild(buildCandidateElement(key));
+        }
+    }
+
+    setupCandidates();
+    return {
+        isCozil: user.data.isCozil,
+        candidates: candidates
+    };
+}
+
+function buildCandidateElement(candidateName) {
+    const div = document.createElement('div');
+    div.classList.add('candidate')
+
+    const span = document.createElement('span');
+    span.classList.add('tooltip-text');
+    span.innerHTML = candidateName;
+
+    div.appendChild(span);
+
+    return div;
+}
+
+function setupCandidates() {
+    const candidateElements = document.getElementsByClassName("candidate");
+
+    for(let i=0; i<candidateElements.length; i++) {
+        candidateElements[i].addEventListener('click', selectCandidate);
+    }
+}
+
+function selectCandidate(e) {
+    const candidateElements = document.getElementsByClassName("candidate");
+    selectedCandidate = e.target;
+
+    for(let i=0; i<candidateElements.length; i++) {
+        candidateElements[i].classList.remove("selected");
+    }
+
+    enableButton();
+    selectedCandidate.classList.add("selected");
+}
+
+function clearCandidate(e) {
+    let candidateElements = document.getElementsByClassName("candidate");
+    selectedCandidate = undefined;
+
+    for(let i=0; i<candidateElements.length; i++) {
+        candidateElements[i].classList.remove("selected");
+    }
+}
 
 async function moveCarousel() {
     const carousel = document.getElementById("carousel");
-    const carouselStyle = getComputedStyle(carousel);
+  
+    if(currentPosition === 0 && await verifyCPF()) {
+        const candidatesObject = (await loadCandidates());
 
-    if(carouselStyle.marginLeft === '0px') {
-        const cpf = getCPF();
-        const voted = await hasVoted(cpf);
+        candidatesList = candidatesObject.candidates;
+        isCozil = candidatesObject.isCozil;
 
-        if(voted === undefined) {
-            enableErrorMessage("CPF não cadastrado");
-            return
-        }
-        if(voted) {
-            enableErrorMessage("CPF já utilizado");
-            return;
-        }
-
-        disableErrorMessage("");
         carousel.style.marginLeft = '-100vw';
+        currentPosition++;
+        return;
     }
-    if(carouselStyle.marginLeft === '-100vw') {
+
+    if(currentPosition === 1 && selectedCandidate != undefined && confirmVote()) {
+        vote(candidatesList, isCozil);
         carousel.style.marginLeft = '-200vw';
+        currentPosition++;
+        return;
     }
-    if(carouselStyle.marginLeft === '-200vw') {
-        carousel.style.marginLeft = '-300vw';
+    if(currentPosition === 2) {
+        clearCPF();
+        disableButton();
+        clearCandidate();
+        carousel.style.marginLeft = '0vw';
+        currentPosition = 0;
+        return;
     }
-    if(carouselStyle.marginLeft === '-300vw') {
-        carousel.style.marginLeft = 'none';
+}
+
+async function verifyCPF() {
+    const cpf = getCPF();
+    const user = await find(cpf);
+
+    if(!user) {
+        enableErrorMessage("CPF não cadastrado");
+        return;
+    }
+
+    const hasUserVoted = await hasVoted(user);
+    
+    if(hasUserVoted) {
+        enableErrorMessage("CPF já utilizado");
+        return;
+    }
+
+    disableErrorMessage("");
+    return true;
+}
+
+
+async function vote(candidates, isCozil) {
+    const _selectedCandidate = selectedCandidate.firstChild.innerHTML;
+    const cpf = getCPF();
+    const user = await find(cpf);
+
+    updateUserVote(user.id);
+    console.log(isCozil);
+    if(isCozil) {
+        console.log("ISCOZIL?")
+        updateCandidateVote('votosCOZIL', _selectedCandidate, candidates[_selectedCandidate]);
+    } else {
+        updateCandidateVote('votosPROINOX', _selectedCandidate, candidates[_selectedCandidate]);
+    }
+
+}
+
+function confirmVote() {
+    const _selectedCandidate = selectedCandidate.firstChild.innerHTML;
+
+    if(confirm("Deseja votar em " + _selectedCandidate + "?")) {
+        return true;
+    } else {
+        return false;
     }
 }
 
 function getCPF() {
     const textField = document.getElementById("text-field");
-
     return textField.value;
+}
+
+function clearCPF() {
+    const textField = document.getElementById("text-field");
+    textField.value = "";
 }
 
 function enableErrorMessage(message) {
@@ -51,6 +217,16 @@ function enableErrorMessage(message) {
 function disableErrorMessage() {
     const errorMessage = document.getElementById("error-message");
     errorMessage.style.color = "rgba(0, 0, 0, 0)";
+}
+
+function enableButton() {
+    const voteButton = document.getElementById("vote");
+    voteButton.removeAttribute('disabled');
+}
+
+function disableButton() {
+    const voteButton = document.getElementById("vote");
+    voteButton.disabled = false;
 }
 
 
